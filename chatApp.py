761ca5e15b4b7f86f374d7ee4db85603927f2057
Gorakh Shinde <gshinde2609@gmail.com>
2026-09-09 23:31:08 +0530

    chatApp.py

--- chatApp.py
+++ chatApp.py
@@ -14,7 +14,7 @@ st.title("💬 Public Ollama Chat Application")
 # )
 
 # Initialize the Ollama Client with the custom host
-client = ollama.Client(host=http://host.docker.internal:11434)
+client = ollama.Client(host="http://host.docker.internal:11434")
 
 # Fetch available models dynamically
 try:
